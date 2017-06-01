7df50886e65ca47251132b3158a267b4483933da
laiafr <laia.fibla.reixachs@gmail.com>
2017-06-01 04:35:04 +0200

    plots

--- big_corpora/conc_cat/4/Dibs-diphone-freq-distribution-PLOTS.R
+++ big_corpora/conc_cat/4/Dibs-diphone-freq-distribution-PLOTS.R
@@ -1,7 +1,10 @@
+
+#Catalan
+
 scan("~/Documents/SegCatSpa/big_corpora/conc_cat/4/spanninga.txt",what="character")->sp
 sp[-1]->sp
 t(matrix(sp,nrow=2))->sp1
-write.table(sp2,"span.txt")
+write.table(sp1,"span.txt")
 read.table("span.txt")->sp
 colnames(sp)<-c("diphone","nsp")
 
@@ -16,3 +19,60 @@ merge(sp,int,all.x=T,all.y=T)->both
 
 both$prop=both$nsp/(both$nsp+both$nint)
 hist(both$prop)
+
+# Density curve
+ggplot(both, aes(x=prop)) + geom_density()
+
+# Spanish
+
+scan("~/Documents/SegCatSpa/big_corpora/conc_cat/4/spanninga.txt",what="character")->sp
+sp[-1]->sp
+t(matrix(sp,nrow=2))->sp1
+write.table(sp1,"span.txt")
+read.table("span.txt")->sp
+colnames(sp)<-c("diphone","nsp")
+
+scan("~/Documents/SegCatSpa/big_corpora/conc_cat/4/internal.txt",what="character")->int
+int[-1]->int
+t(matrix(int,nrow=2))->int
+write.table(int,"int.txt")
+read.table("int.txt")->int
+colnames(int)<-c("diphone","nint")
+
+merge(sp,int,all.x=T,all.y=T)->both
+
+both$prop=both$nsp/(both$nsp+both$nint)
+hist(both$prop)
+
+# Density curve
+ggplot(both, aes(x=prop)) + geom_density()
+
+# Bilingual 
+
+scan("~/Documents/SegCatSpa/big_corpora/conc_cat/4/spanninga.txt",what="character")->sp
+sp[-1]->sp
+t(matrix(sp,nrow=2))->sp1
+write.table(sp1,"span.txt")
+read.table("span.txt")->sp
+colnames(sp)<-c("diphone","nsp")
+
+scan("~/Documents/SegCatSpa/big_corpora/conc_cat/4/internal.txt",what="character")->int
+int[-1]->int
+t(matrix(int,nrow=2))->int
+write.table(int,"int.txt")
+read.table("int.txt")->int
+colnames(int)<-c("diphone","nint")
+
+merge(sp,int,all.x=T,all.y=T)->both
+
+both$prop=both$nsp/(both$nsp+both$nint)
+hist(both$prop)
+
+# Density curve
+ggplot(both, aes(x=prop)) + geom_density()
+
+# Plot them all 
+
+# Density plots with semi-transparent fill
+ggplot(both, aes(x=prop, fill=cond)) + geom_density(alpha=.3)
+
