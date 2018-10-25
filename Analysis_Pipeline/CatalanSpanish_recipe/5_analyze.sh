e15551bfb3c7954b19ad3e66c55e84c13d43d3a1
alecristia <alecristia@gmail.com>
2018-10-25 13:11:54 +0200

    updated

--- Analysis_Pipeline/CatalanSpanish_recipe/5_analyze.sh
+++ Analysis_Pipeline/CatalanSpanish_recipe/5_analyze.sh
@@ -6,54 +6,64 @@
 
 ######### VARIABLES ###############
 
-DATAFOLDER=$1
-RESFOLDER=$2
-
-PIPELINE="/fhgfs/bootphon/scratch/lfibla/CDSwordSeg/algoComp/segment_CatSpa.py"
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cm/shared/apps/boost/1.62.0/stage/lib
+input_dir=$1
+output_dir=$2
 
 ##################################
 
+# wordseg tool to launch segmentation pipelines on the cluster
+wordseg_slurm="/shared/apps/wordseg/tools/wordseg-slurm.sh"
 
-# Run all algos in the cluster writing 'all' next to algorithms.
-# To just run one single algorithm choose from 'AGc3sf', 'AGu', 'dibs', 'dmcmc', 'ngrams', 'puddle', 'TPs', 'all'
+# the token separators in the tags files
+separator="-p' ' -s';esyll' -w';eword'"
 
-module load python-anaconda
 
-mkdir -p ${RESFOLDER}/
+# get the list of tags files in the input_dir, assuming that what gets passed is a folder containing tags (i.e. no embedding)
+all_tags="$input_dir/*tags.txt"
+ntags=$(echo $all_tags | wc -w)
+echo "found $ntags tags files in $input_dir"
 
-for VERSION in $DATAFOLDER/*
+# temporary jobs file to list all the wordseg jobs to execute
+jobs=$(mktemp)
+trap "rm -rf $jobs" EXIT
+
+# build the list of wordseg jobs from the list of tags files
+counter=1
+for tags in $all_tags
 do
-    if [ -d $VERSION ]
-    then
+    name=$(basename $tags | cut -d- -f1)
+    echo -n "[$counter/$ntags] building jobs for $name ..."
 
-        VNAME=`basename ${VERSION#$RESFOLDER}`
-        echo Clusterizing ${VNAME}
-        $PIPELINE --goldfile ${VERSION}/gold.txt \
-                  --output-dir ${RESFOLDER}/${VNAME} \
-                  --algorithms AGc3sf \
-                  --ag-median 1 \
-                  --clusterize \
-                  --jobs-basename s${VNAME} \
-                  ${VERSION}/tags.txt || exit 1
-    fi
+
+    # defines segmentation jobs
+    echo "$name-syllable-baseline-00 $tags syllable $separator wordseg-baseline -v -P 0" >> $jobs
+    echo "$name-syllable-baseline-10 $tags syllable $separator wordseg-baseline -v -P 1" >> $jobs
+    echo "$name-syllable-tprel $tags syllable $separator wordseg-tp -v -t relative" >> $jobs
+    echo "$name-syllable-tpabs $tags syllable $separator wordseg-tp -v -t absolute" >> $jobs
+    echo "$name-phone-dibs $tags phone $separator wordseg-dibs -v -t phrasal -u phone $tags" >> $jobs
+    echo "$name-phone-puddle $tags phone $separator wordseg-puddle -v -j 5 -w 2" >> $jobs
+    echo "$name-syllable-ag $tags syllable $separator wordseg-ag -vv -j 8" >> $jobs
+
+    ((counter++))
+    echo " done"
+
+    # # for testing, process only some tags
+    # [ $counter -eq 4 ] && break
 done
 
 
+# load the wordseg python environment
+module load anaconda/3
+source activate /shared/apps/anaconda3/envs/wordseg
 
-# To not pass thought the cluster (just to use in emergecy case and for light algos)
-#echo $PIPELINE --goldfile $VERSION/gold.txt --output-dir $RESFOLDER/$VNAME --algorithms TPs $VERSION/tags.txt || exit 1
+# launching all the jobs
+echo -n "submitting $(cat $jobs | wc -l) jobs ..."
+$wordseg_slurm $jobs $output_dir > /dev/null
+echo " done"
 
-#        VNAME=`basename ${VERSION}`
-#        echo Clusterizing $VNAME
-#        $PIPELINE $VERSION/tags.txt \
-#                  --goldfile $VERSION/gold.txt \
-#                  --output-dir $RESFOLDER/$VNAME \
-#                  --algorithms TPs dibs \
-#                   --clusterize=False \
-#                   --jobs-basename $VERSION \
-#                  --ag-median 5 \
-#            || exit 1
-#    fi
-#done
+echo "all jobs submitted, writing to $output_dir"
+echo "view status with 'squeue -u $USER'"
+
+# unload the environment
+source deactivate
+
